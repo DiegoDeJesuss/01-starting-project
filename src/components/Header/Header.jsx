5b464eaf71b54f1a148864bff96e08e71bed0a45
DiegoDeJesuss <diegodejesuspaula@hotmail.com>
2026-07-09 21:10:19 -0300

    teste

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,10 +10,10 @@ export default function Header() {
   return (
     <header>
       <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
+      <h1>React Essentials :)</h1>
       <p>
         {description} React concepts you will need for almost any app you are
-        going to build!
+        going to build!!
       </p>
     </header>
   );
